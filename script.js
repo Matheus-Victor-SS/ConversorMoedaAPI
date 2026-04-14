f418478d340239688f756261443453d5c61d13bb
Matheus <matheus.victor.ss48@gmail.com>
2026-04-14 10:44:03 -0300

    linkando tudo e melhorando a requisição

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 function converter(){
-    document.getElementById("res")
     var valor = document.getElementById("quant").value
     var moedaum = document.getElementById("moeda1").value
     var moedadois = document.getElementById("moeda2").value
@@ -7,11 +6,12 @@ function converter(){
 fetch(`https://api.freecurrencyapi.com/v1/latest?apikey=fca_live_j1a1wClyBw4a9HRLVFWtNAVZhqa0tYz6Sn07titf`)
 .then(res => res.json())
 .then(dados => {
+    console.log(dados)
     let taxa = dados.data[moedadois]
-            let resultado = valor * taxa
+            let res = valor * taxa
 
             document.getElementById("res").innerText =
-                `${valor} ${moedaum} = ${resultado.toFixed(2)} ${moedadois}`
+                `${valor} ${moedaum} = ${res.toFixed(2)} ${moedadois}`
         })
         .catch(erro => {
             console.log("Erro:", erro)
